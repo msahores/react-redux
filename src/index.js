77d6fa003eeb8b269a7703f95d37c0a430a95cd7
manuel <manuel@sahores.com>
2020-05-12 16:05:47 -0300

    Addind First Middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import listReducer from './store/reducers/list';
@@ -12,7 +12,19 @@ const rootReducer = combineReducers({
   lst: listReducer
 })
 
-const store = createStore(rootReducer);
+//Adding a middleware
+const logger = store => {
+   return next => {
+      return action => {
+        console.log('[Middleware] Dispatching', action)
+        const result = next(action);
+        console.log('[Middleware] next state', store.getState());
+        return result;
+      }
+   }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger)); // Enhancer: apply middleware, can add more middlewares
 
 ReactDOM.render(
   <React.StrictMode>
